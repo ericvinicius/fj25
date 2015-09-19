050aab751ac33080c68d7e5dff3a3e0342446952
ericvinicius <eric.camargo@gmail.com>
2015-09-19 12:25:29 -0300

    Criando a base do projeto

---
+++ src/br/com/caelum/financas/teste/TestaInsereConta.java
@@ -0,0 +1,32 @@
+package br.com.caelum.financas.teste;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import br.com.caelum.financas.modelo.Conta;
+
+public class TestaInsereConta {
+
+	public static void main(String[] args) {
+		
+		EntityManagerFactory factory = Persistence.createEntityManagerFactory("controlefinancas");
+		
+		EntityManager manager = factory.createEntityManager();
+		
+		Conta conta = new Conta();
+		conta.setTitular("Ze Mane 2");
+		conta.setBanco("Banco do Brasil");
+		conta.setNumero("123456-6");
+		conta.setAgencia("0999");
+		
+		manager.getTransaction().begin();
+		manager.persist(conta);
+		manager.getTransaction().commit();
+		manager.close();
+		
+		System.out.println("Conta Gravada Com Sucesso!");
+		
+	}
+
+}
